82847632f350c7c33418f41c821edd21f6928b8c
Bhavya Patel <89627115+BHAVYAP1811@users.noreply.github.com>
2026-03-10 16:24:46 +0530

    Initial website commit

---
+++ first.js
@@ -0,0 +1,2 @@
+document.getElementById("date").innerHTML = new Date().toDateString();
+alert("Welcome to my website! I hope you find it informative and enjoyable. Feel free to explore and learn more about me and my work. If you have any questions or would like to connect, don't hesitate to reach out through the contact page. Thank you for visiting!");
